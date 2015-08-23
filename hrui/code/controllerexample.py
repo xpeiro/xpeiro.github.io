cdce4e59421c39b515583db26126d003bda8c397
Daniel Peiró <danipeiro@gmail.com>
2015-08-23 13:04:26 +0200

    Finish controller example

--- hrui/code/controllerexample.py
+++ hrui/code/controllerexample.py
@@ -7,3 +7,18 @@ mongoclient = MongoClient("127.0.0.1")
 db = mongoclient.hrui
 data = db.data
 #end MongoDB setup
+
+# read joystick item from database
+joystick = data.find_one({"item": "joystick"})
+
+#use joystick items
+x = float(joystick['x'])
+y = float(joystick['y'])
+print("X: " + str(x))
+print("Y: " + str(y))
+
+# write robotData item
+data.update({"item": "robotData"}, {"$set":{"position": {"x": x, "y": y}}})
+
+# create new item (or update it if it exists)
+data.update({"item": "newItem"}, {"$set":{"x": x, "y": y}}, upsert=True)
